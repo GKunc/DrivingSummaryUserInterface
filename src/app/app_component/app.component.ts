e8ef882aef2b0c46e749148aabae5098dffd65d8
Grzegorz Kunc <grzeg.kunc@gmail.com>
2019-11-14 18:01:55 +0100

    Add good bootstrap

--- src/app/app_component/app.component.ts
+++ src/app/app_component/app.component.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'DrivingSummaryUserInterface';
